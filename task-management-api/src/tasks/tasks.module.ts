343bbc4c22d181c9f162f73fdb3f44d09276ad0c
willianfalbo <willianfalbo@gmail.com>
2020-01-06 02:32:05 -0300

    section 5 - authentication - setting up JWT/Passport.js

--- task-management-api/src/tasks/tasks.module.ts
+++ task-management-api/src/tasks/tasks.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskRepository])
+    TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
